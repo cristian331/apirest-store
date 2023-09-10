cacb8a146d6d45ac3d730b31c383d778dd9297bd
cristian331 <cristian.a0661@gmail.com>
2023-09-09 19:09:19 -0500

    config .env para variable de ingreso

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,12 +1,22 @@
 //conexion a terceros-- DataBase
-const { Pool } = require('pg')
+const { Pool } = require('pg');
+const { config } = require('./../config/config');
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'cristian',
-  password: 'admin123',
-  database: 'my_store'
-});
+// const pool = new Pool({
+//   host:
+//   port:
+//   user:
+//   password:
+//   database:
+// });
+
+//con informacion insertada en variables de entorno
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+
+
+const pool = new Pool({ connectionString: URI});
 
 module.exports = pool;
